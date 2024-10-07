fb1951e545149cf7616f79be99a3bd2d9e707ec5
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2024-10-08 01:54:13 +0300

    feat: implement zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const file = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const archive = path.join(__dirname, 'files', 'archive.gz');
+
+    const readStream = fs.createReadStream(file, 'utf-8');
+    const writeStream = fs.createWriteStream(archive);
+
+    const gzip = createGzip();
+
+    readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
