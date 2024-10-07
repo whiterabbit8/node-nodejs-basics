f90913b501717216e98e33f52db21a4dfd733e27
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2024-10-08 01:35:38 +0300

    feat: implement streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const file = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    const readStream = fs.createReadStream(file);
+
+    readStream.on('data', (chunk) => process.stdout.write(chunk + '\n'));
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from 'stream';
+
+const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+        callback(null, chunk.toString().split('').reverse().join(''));
+    }
+});
+
 const transform = async () => {
-    // Write your code here 
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
