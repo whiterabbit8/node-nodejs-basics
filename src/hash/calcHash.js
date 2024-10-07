1cbed81db5c7d6bf06876bc6482e302b5ac1d714
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2024-10-08 01:10:41 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const file = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const readStream = fs.createReadStream(file);
+    const hash = createHash('sha256');
+
+    readStream.on('data', (chunk) => hash.update(chunk));
+    readStream.on('end', () => console.log(hash.digest('hex')));
 };
 
 await calculateHash();
