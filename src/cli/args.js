ae1c27cd2c4471e6daf443cb767c970976d191f5
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2024-10-08 00:15:43 +0300

    feat: implement cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argsArr = process.argv.slice(2);
+    const argsObj = {};
+
+    for (let i = 0; i < argsArr.length; i += 2) {
+        argsObj[argsArr[i].slice(2)] = argsArr[i + 1];
+    }
+
+    console.log(
+        Object.entries(argsObj)
+            .map(([key, val]) => `${key} is ${val}`)
+            .join(', ')
+        );
 };
 
 parseArgs();
